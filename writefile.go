1065d293395f5745a2fed33aecd4e4521127917a
whyrusleeping <why@ipfs.io>
2019-11-04 10:24:42 -0800

    improve writefile

--- writefile.go
+++ writefile.go
@@ -1,26 +1,40 @@
 package typegen
 
 import (
+	"bytes"
+	"go/format"
 	"os"
 
 	"golang.org/x/xerrors"
 )
 
 func WriteTupleEncodersToFile(fname, pkg string, types ...interface{}) error {
+	buf := new(bytes.Buffer)
+
+	if err := PrintHeaderAndUtilityMethods(buf, pkg); err != nil {
+		return xerrors.Errorf("failed to write header: %w", err)
+	}
+
+	for _, t := range types {
+		if err := GenTupleEncodersForType(pkg, t, buf); err != nil {
+			return xerrors.Errorf("failed to generate encoders: %w", err)
+		}
+	}
+
+	data, err := format.Source(buf.Bytes())
+	if err != nil {
+		return err
+	}
+
 	fi, err := os.Create(fname)
 	if err != nil {
 		return xerrors.Errorf("failed to open file: %w", err)
 	}
 	defer fi.Close()
 
-	if err := PrintHeaderAndUtilityMethods(fi, pkg); err != nil {
-		return xerrors.Errorf("failed to write header: %w", err)
-	}
-
-	for _, t := range types {
-		if err := GenTupleEncodersForType(pkg, t, fi); err != nil {
-			return xerrors.Errorf("failed to generate encoders: %w", err)
-		}
+	_, err = fi.Write(data)
+	if err != nil {
+		return err
 	}
 
 	return nil
