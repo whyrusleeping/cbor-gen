5189b25503c97a32e1e469afd91f148f9d45d38d
Brian Olson <brian.olson@gmail.com>
2024-08-02 16:58:33 -0400

    optionally sort type names in generated code file also, allow *Foo{} demo object inputs

--- utils.go
+++ utils.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"io"
 	"math"
+	"reflect"
+	"sort"
 	"sync"
 
 	cid "github.com/ipfs/go-cid"
@@ -486,4 +488,44 @@ func WriteCidBuf(buf []byte, w io.Writer, c cid.Cid) error {
 	}
 
 	return nil
+}
+
+// sort type example objects on name of type
+func sortTypeNames(obs []any) []any {
+	temp := make([]tnAny, len(obs))
+	for i, ob := range obs {
+		v := reflect.ValueOf(ob)
+		if v.Kind() == reflect.Pointer {
+			v = v.Elem()
+		}
+		temp[i] = tnAny{v.Type().Name(), ob}
+	}
+	sortref := tnAnySorter(temp)
+	sort.Sort(&sortref)
+	out := make([]any, len(obs))
+	for i, rec := range temp {
+		out[i] = rec.ob
+	}
+	return out
+}
+
+// type-name and any
+type tnAny struct {
+	name string
+	ob   any
+}
+
+type tnAnySorter []tnAny
+
+// sort.Interface
+func (tas *tnAnySorter) Len() int {
+	return len(*tas)
+}
+func (tas *tnAnySorter) Less(i, j int) bool {
+	return (*tas)[i].name < (*tas)[j].name
+}
+func (tas *tnAnySorter) Swap(i, j int) {
+	t := (*tas)[i]
+	(*tas)[i] = (*tas)[j]
+	(*tas)[j] = t
 }
